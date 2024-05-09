a617feafb553ae9e3fe4fae41a141e58d7ddb380
MohamedAbotalb <mohamed.abotalb277@gmail.com>
2024-05-09 11:01:45 +0300

    Add department component in the home page

---
+++ src/assets/data/departments.ts
@@ -0,0 +1,26 @@
+export const departments = [
+  {
+    id: 1,
+    name: 'Computer Science',
+    location: 'Building A',
+    head: 'Prof. Ayman',
+  },
+  {
+    id: 2,
+    name: 'Engineering',
+    location: 'Building B',
+    head: 'Prof. Mohamed',
+  },
+  {
+    id: 3,
+    name: 'Biology',
+    location: 'Building C',
+    head: 'Prof. Ali',
+  },
+  {
+    id: 4,
+    name: 'Physics',
+    location: 'Building D',
+    head: 'Prof. Mahmoud',
+  },
+];
---
+++ src/models/department.ts
@@ -0,0 +1,36 @@
+export class Department {
+  constructor(
+    public id: number,
+    public name: string,
+    public head: string,
+    public location: string
+  ) {}
+
+  // set Id(id: number) {
+  //   this.id = id;
+  // }
+  // get Id(): number {
+  //   return this.id;
+  // }
+
+  // set Name(name: string) {
+  //   this.name = name;
+  // }
+  // get Name(): string {
+  //   return this.name;
+  // }
+
+  // set Head(head: string) {
+  //   this.head = head;
+  // }
+  // get Head(): string {
+  //   return this.head;
+  // }
+
+  // set Location(location: string) {
+  //   this.location = location;
+  // }
+  // get Location(): string {
+  //   return this.location;
+  // }
+}
